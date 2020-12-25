d852c0328aa84598413a28a152aa44670644a4e2
GuillaumeSavin <guisav1996@hotmail.fr>
2020-12-25 20:31:02 +0100

    OBJ_QUESTION-01 Ajout objet + attributs + constructeur

---
+++ EnvWinProjectQuizz/EnvWinProjectQuizz.Android/Question.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnvWinProjectQuizz.Droid
+{
+    class Question
+    {
+        String enonce;
+        List<Reponse> reponsesPossibles;
+        List<int> correction;
+
+        public Question(String p_enonce, List<Reponse> p_reponsesPossibles, List<int> p_correction)
+        {
+            this.enonce = p_enonce;
+            this.reponsesPossibles = p_reponsesPossibles;
+            this.correction = correction;
+        }
+    }
+}
