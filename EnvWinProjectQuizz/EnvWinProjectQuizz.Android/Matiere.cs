65cf80c1751e1bc151fe32e820e8d3c078e0e3a7
GuillaumeSavin <guisav1996@hotmail.fr>
2020-12-25 19:25:14 +0100

    OBJ_MATIERE-01 Ajout objet + attributs + constructeur

---
+++ EnvWinProjectQuizz/EnvWinProjectQuizz.Android/Matiere.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnvWinProjectQuizz.Droid
+{
+    class Matiere
+    {
+        List<Partie> listePartie;
+
+        public Matiere(List<Partie> p_listePartie)
+        {
+            this.listePartie = p_listePartie;
+        }
+    }
+}
