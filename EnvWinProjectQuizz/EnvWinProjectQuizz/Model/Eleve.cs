208f54250c6dca28255ca6a750dea0901fa4ef42
PegCalde <calderonpeg@gmail.com>
2020-12-29 12:14:10 +0100

    rangement code

---
+++ EnvWinProjectQuizz/EnvWinProjectQuizz/Model/Eleve.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnvWinProjectQuizz
+{
+    public class Eleve
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ EnvWinProjectQuizz/EnvWinProjectQuizz/ViewModel/EleveViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace EnvWinProjectQuizz.ViewModel
+{
+    class EleveViewModel
+    {
+        public IList<Eleve> EleveList { get; set; }
+
+        public EleveViewModel()
+        {
+            EleveList = new ObservableCollection<Eleve>();
+
+            EleveList.Add(new Eleve
+            {
+                Name = "Illidan"
+            });
+            EleveList.Add(new Eleve
+            {
+                Name = "Jaina"
+            });
+            EleveList.Add(new Eleve
+            {
+                Name = "Anduin"
+            });
+            EleveList.Add(new Eleve
+            {
+                Name = "Varian"
+            });
+            EleveList.Add(new Eleve
+            {
+                Name = "Thrall"
+            });
+        }
+    }
+}
