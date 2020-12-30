d906d561b0b6cf26bf1edb45fc157b5e78bed8c0
GuillaumeSavin <guisav1996@hotmail.fr>
2020-12-30 11:15:32 +0100

    master-02 Add UWP project + adapt cs files to new project

--- EnvWindowsProjectQuizz/ClassesMétiers/Question.cs
+++ EnvWindowsProjectQuizz/ClassesMétiers/Question.cs
@@ -1,15 +1,10 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace EnvWinProjectQuizz.Droid
+namespace EnvWindowsProjectQuizz
 {
     class Question
     {
