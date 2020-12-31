2ba52e03c71a82a75f4e99adaf9844508623937d
pegCalde <calderonpeg@gmail.com>
2020-12-31 17:23:01 +0100

    accueil fr début

--- EnvWindowsProjectQuizz/MainPage.xaml.cs
+++ EnvWindowsProjectQuizz/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace EnvWindowsProjectQuizz
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
