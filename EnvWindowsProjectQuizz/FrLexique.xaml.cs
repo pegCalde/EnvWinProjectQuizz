995cc022f29aa8399ac3b20b713825500a5ef050
pegCalde <calderonpeg@gmail.com>
2021-01-01 18:56:29 +0100

    retour acueil

--- EnvWindowsProjectQuizz/FrLexique.xaml.cs
+++ EnvWindowsProjectQuizz/FrLexique.xaml.cs
@@ -1,20 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+
 
 namespace EnvWindowsProjectQuizz
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class FrLexique : ContentPage
+    public partial class FrLexique : Page
     {
         public FrLexique()
         {
             InitializeComponent();
+        }
+
+        private void ReturnHome_Tapped(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
