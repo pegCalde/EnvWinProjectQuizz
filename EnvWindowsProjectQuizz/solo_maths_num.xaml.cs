8bde410bc76af841ecc81afe8fce82b1a508d3a6
pegCalde <calderonpeg@gmail.com>
2021-01-01 17:34:52 +0100

    page numérotation

--- EnvWindowsProjectQuizz/solo_maths_num.xaml.cs
+++ EnvWindowsProjectQuizz/solo_maths_num.xaml.cs
@@ -20,9 +20,9 @@ namespace EnvWindowsProjectQuizz
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
     /// </summary>
-    public sealed partial class solo_maths_mesures : Page
+    public sealed partial class solo_maths_num : Page
     {
-        public solo_maths_mesures()
+        public solo_maths_num()
         {
             this.InitializeComponent();
         }
